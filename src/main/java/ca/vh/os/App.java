cd1e79ccb6aabb866f4a5b625295db3216c63d24
Oleg Suvorov <ol.suvorov@gmail.com>
2019-11-20 00:09:47 +0100

    Initial commit. Created project

---
+++ src/main/java/ca/vh/os/App.java
@@ -0,0 +1,13 @@
+package ca.vh.os;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
